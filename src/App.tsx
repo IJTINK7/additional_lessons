56ba0a99a74c285ee8508272c1a0982bb2147e0e
IJTINK7 <ijtink7@mail.ru>
2023-09-21 20:52:18 +0300

    Using "useEffect" hook with Array containing several dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-    const [data, setData] = useState(true);
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => console.log(json))
-    }, [data])
-const changeData = () =>{
-    setData(!data)
-}
-  return (
-    <div className="App">
-        <button onClick={changeData}>Change data</button>
-    </div>
-  );
+	const [data1, setData1] = useState(true);
+	const [data2, setData2] = useState(true);
+	const [data3, setData3] = useState(true);
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos/7')
+			.then(response => response.json())
+			.then(json => console.log(json))
+	}, [data1, data2, data3])
+
+	const changeData1 = () => {
+		setData1(!data1)
+	}
+	const changeData2 = () => {
+		setData2(!data2)
+	}
+	const changeData3 = () => {
+		setData3(!data3)
+	}
+	return (
+		<div className="App">
+			<button onClick={changeData1}>Change data 1</button>
+			<button onClick={changeData2}>Change data 2</button>
+			<button onClick={changeData3}>Change data 3</button>
+		</div>
+	);
 }
 
 export default App;
