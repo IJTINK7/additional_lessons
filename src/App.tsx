3023c2246f9cfa47082245499c9be7caf6354db5
IJTINK7 <ijtink7@mail.ru>
2023-09-21 20:22:41 +0300

    Preparing for 01-1 additional lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(1)
+  console.log(2)
+  console.log(3)
+  console.log(4)
+  console.log(5)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
