4bcd9f2ab69ab893a7aab934342c42a55e5b93f9
IJTINK7 <ijtink7@mail.ru>
2023-09-21 20:49:30 +0300

    Using "useEffect" hook with Array containing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+    const [data, setData] = useState(true);
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
             .then(json => console.log(json))
-    }, [])
-
+    }, [data])
+const changeData = () =>{
+    setData(!data)
+}
   return (
     <div className="App">
-
+        <button onClick={changeData}>Change data</button>
     </div>
   );
 }
