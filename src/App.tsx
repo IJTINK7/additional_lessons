6b1d372f1c41954dc5383829a8c47b019ebfbfc5
IJTINK7 <ijtink7@mail.ru>
2023-09-21 20:26:26 +0300

    Adding async request from fake server (link: https://jsonplaceholder.typicode.com/)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   console.log(1)
   console.log(2)
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
   console.log(3)
   console.log(4)
   console.log(5)
