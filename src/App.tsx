e35692cbba998c1346a040f53bfff7e4a48571f9
IJTINK7 <ijtink7@mail.ru>
2023-09-21 20:43:35 +0300

    Adding "useEffect" hook with Dependency array

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
-  console.log(1)
-  console.log(2)
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }, [])
 
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-
-  console.log(3)
-  console.log(4)
-  console.log(5)
   return (
     <div className="App">
 
