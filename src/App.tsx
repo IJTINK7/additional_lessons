b9d6eb38d8e61e0a70abc02625a4bfc046aeae42
IJTINK7 <ijtink7@mail.ru>
2023-09-22 09:45:55 +0300

    Finishing 01-1 additional lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
 import {Input} from "./components/Input";
-
 type TodosType = {
 	userId: number
 	id: number
 	title: string
 	completed: boolean
 }
-
 function App() {
+	// const [data, setData] = useState(true);
 	const [todos, setTodos] = useState<TodosType[]>([]);
 	console.log(todos)
-
-	// const [inputValue, setInputValue] = useState("")
-	const inputValue = useRef<HTMLInputElement>(null)
-
+	const [inputValue, setInputValue] = useState("")
 	const fetchFoo = () => {
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then(response => response.json())
@@ -25,43 +21,36 @@ function App() {
 	useEffect(() => {
 		fetchFoo()
 	}, [])
-
 	// useEffect(() => {
 	// 	fetch('https://jsonplaceholder.typicode.com/todos/7') // It's just an object !!!
 	// 		.then(response => response.json())
 	// 		.then(json => setTodos([json])) // don't forget about array of objects !!!
 	// }, [])
-
-
 	// const changeData1 = () => {
 	// 	setData(!data)
 	// }
-
 	const showData = () => {
 		fetchFoo()
 	}
 	const hideData = () => {
 		setTodos([])
 	}
-	const postData = () => {
-		if(inputValue.current) {
-			const newTodo = {
-				userId: 1,
-				id: todos.length + 1,
-				title: inputValue.current.value,
-				completed: false
-			}
-			setTodos([newTodo, ...todos])
-			inputValue.current.value = ""
+	const postData = (value: string) => {
+		const newTodo = {
+			userId: 1,
+			id: todos.length + 1,
+			title: value,
+			completed: false
 		}
+		setTodos([newTodo, ...todos])
+		setInputValue("")
 	}
-
-
 	return (
 		<div className="App">
+			{/*<Button name={"Change data 1"} callBack={changeData1}/>*/}
 			<div>
-				<Input inputValue={inputValue} />
-				<Button name={"Add data"} callBack={postData}/>
+				<Input inputValue={inputValue} setInputValue={setInputValue}/>
+				<Button name={"Add data"} callBack={()=>postData(inputValue)}/>
 			</div>
 			<Button name={"Show data"} callBack={showData}/>
 			<Button name={"Hide data"} callBack={hideData}/>
@@ -78,5 +67,87 @@ function App() {
 		</div>
 	);
 }
-
 export default App;
+// useRef
+// import React, {useEffect, useRef, useState} from 'react';
+// import './App.css';
+// import {Button} from "./components/Button";
+// import {Input} from "./components/Input";
+//
+// type TodosType = {
+// 	userId: number
+// 	id: number
+// 	title: string
+// 	completed: boolean
+// }
+//
+// function App() {
+// 	const [todos, setTodos] = useState<TodosType[]>([]);
+// 	console.log(todos)
+//
+// 	// const [inputValue, setInputValue] = useState("")
+// 	const inputValue = useRef<HTMLInputElement>(null)
+//
+// 	const fetchFoo = () => {
+// 		fetch('https://jsonplaceholder.typicode.com/todos')
+// 			.then(response => response.json())
+// 			.then(json => setTodos(json))
+// 	}
+// 	useEffect(() => {
+// 		fetchFoo()
+// 	}, [])
+//
+// 	// useEffect(() => {
+// 	// 	fetch('https://jsonplaceholder.typicode.com/todos/7') // It's just an object !!!
+// 	// 		.then(response => response.json())
+// 	// 		.then(json => setTodos([json])) // don't forget about array of objects !!!
+// 	// }, [])
+//
+//
+// 	// const changeData1 = () => {
+// 	// 	setData(!data)
+// 	// }
+//
+// 	const showData = () => {
+// 		fetchFoo()
+// 	}
+// 	const hideData = () => {
+// 		setTodos([])
+// 	}
+// 	const postData = () => {
+// 		if(inputValue.current) {
+// 			const newTodo = {
+// 				userId: 1,
+// 				id: todos.length + 1,
+// 				title: inputValue.current.value,
+// 				completed: false
+// 			}
+// 			setTodos([newTodo, ...todos])
+// 			inputValue.current.value = ""
+// 		}
+// 	}
+//
+//
+// 	return (
+// 		<div className="App">
+// 			<div>
+// 				<Input inputValue={inputValue} />
+// 				<Button name={"Add data"} callBack={postData}/>
+// 			</div>
+// 			<Button name={"Show data"} callBack={showData}/>
+// 			<Button name={"Hide data"} callBack={hideData}/>
+// 			<div>{todos.map(el => {
+// 				return (
+// 					<div key={el.id}>
+// 						<input type="checkbox" checked={el.completed}/>
+// 						<span> <b>title:</b> {el.title} </span>
+// 						<span> <b>id:</b> {el.id} </span>
+// 					</div>
+// 				)
+// 			})}
+// 			</div>
+// 		</div>
+// 	);
+// }
+//
+// export default App;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,33 @@
-import React, {RefObject} from 'react';
+import React, {ChangeEvent} from 'react';
 
 type PropsType = {
-	inputValue: RefObject<HTMLInputElement>
+	inputValue: string
+	setInputValue: (newTitle: string)=> void
 }
 
-export const Input: React.FC<PropsType> = ({inputValue}) => {
+export const Input: React.FC<PropsType> = ({inputValue, setInputValue}) => {
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setInputValue(e.currentTarget.value)
+	}
 	return (
 		<div>
-			<input type="text" ref={inputValue}/>
+			<input value={inputValue} type="text" onChange={onChangeHandler}/>
 		</div>
 	);
 };
+
+//useRef
+
+// import React, {RefObject} from 'react';
+//
+// type PropsType = {
+// 	inputValue: RefObject<HTMLInputElement>
+// }
+//
+// export const Input: React.FC<PropsType> = ({inputValue}) => {
+// 	return (
+// 		<div>
+// 			<input type="text" ref={inputValue}/>
+// 		</div>
+// 	);
+// };
