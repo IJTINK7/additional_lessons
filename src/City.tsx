407b7cb23a0f6592d8a488df4066ff727206207a
IJTINK7 <ijtink7@mail.ru>
2023-09-26 11:00:01 +0300

    Avoiding styles for display banknotes

--- src/City.tsx
+++ src/City.tsx
@@ -1,52 +1,24 @@
 import React from 'react';
-import {CurrentBankomat} from "./CurrentBankomat";
 import {MoneyType} from "./App";
+import {CurrentBankomat} from "./CurrentBankomat";
+import styled from "styled-components";
+
 
 type CityPropsType = {
-    data: MoneyType[] //встречаем денюжки
+	data: MoneyType[] //встречаем денюжки
 }
 
 export const City = (props: CityPropsType) => {
+	const mappedMoney = props.data.map((el, index)=>{
+		return <CurrentBankomat key={index} money={el}/>
+	})
+	return(
+		<Wrapper>{mappedMoney}</Wrapper>
+	);
+}
 
-    // пока это пропускам
-    const mappedMoney = props.data.map((el: MoneyType, index) => (
-        <CurrentBankomat
-            key={index}
-            money={el}
-        />
-    ))
-
-
-    return (
-        <div>
-            {/*<div>*/}
-                {/*<ul>*/}
-                {/*    {props.data.map((el,index)=>{*/}
-                {/*        return(*/}
-                {/*            <li key={index}>*/}
-                {/*                <span>{el.number} </span>*/}
-                {/*                <span>{el.value} </span>*/}
-                {/*                <span>{el.banknotes} </span>*/}
-                {/*            </li>*/}
-                {/*        )*/}
-                {/*    })}*/}
-                {/*</ul>*/}
-            {/*</div>*/}
-            <div>{mappedMoney}</div>
-        </div>
-    );
-};
-//1
-// Вроде все норм, но нужно причесать код. Давайте создадим const mappedMoney = props.data.map(el=>el...)
-// Т.е. нам нужно вынести map из вертски, оставив в верстке только mappedMoney
-
-//2
-// Отлично, но теперь давайте совсем все сделаем по красоте. Вынесем верстку в отдельную компоненту и ее замапим!
-// Ну и наконец давайте версточку натянем, для этого будем использовать StyledComponents
-// yarn add styled-components
-// yarn add @types/styled-components
-
-//3
-// Вроде все норм, ну точнее почти норм- дублирование-это грех. Хотелось бы от него избавиться.
-// И StyledComponents нам в этом отлично поможет, ведь он может принимать пропсы!
-// Как это сделать в документашке
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 30px;
+`
--- src/CurrentBankomat.tsx
+++ src/CurrentBankomat.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CurrentBankomatPropsType = {
     money: MoneyType
@@ -7,8 +8,45 @@ type CurrentBankomatPropsType = {
 export const CurrentBankomat = (props:CurrentBankomatPropsType) => {
 
     return (
-        <div>
-            {props.money.banknotes}
-        </div>
+        props.money.banknotes === "Dollars"
+            ? <BanknoteGreen>
+                <Name>{props.money.banknotes}</Name>
+                <Nominal>{props.money.value}</Nominal>
+            </BanknoteGreen>
+            : <BanknoteBlue>
+                <Name>{props.money.banknotes}</Name>
+                <Nominal>{props.money.value}</Nominal>
+            </BanknoteBlue>
     );
 };
+
+
+const BanknoteContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    width: 400px;
+    height: 200px;
+    margin: 10px;
+`;
+
+const BanknoteGreen = styled(BanknoteContainer)`
+    background-color: green;
+`;
+
+const BanknoteBlue = styled(BanknoteContainer)`
+  background-color: #169bcb;
+`;
+
+const Name = styled.div`
+    display: flex;
+    align-content: center;
+    font-size: 30px;
+`
+const Nominal = styled.div`
+    display: flex;
+    align-content: center;
+    font-size: 90px;
+`
