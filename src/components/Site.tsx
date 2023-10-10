5ebfeb330a4648963eec12b0b794729fe23b5381
IJTINK7 <ijtink7@mail.ru>
2023-10-10 20:45:19 +0300

    Using styled components

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -5,7 +5,7 @@ import {PageOne} from "./pages/PageOne";
 import {PageTwo} from "./pages/PageTwo";
 import {PageThree} from "./pages/PageThree";
 import {Error404} from "./pages/Error404";
-
+import styled from "styled-components";
 
 export const Site = () => {
     return (
@@ -13,9 +13,9 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page1"}>Page 1</NavLink></div>
-                    <div><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page2"}>Page 2</NavLink></div>
-                    <div><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page3"}>Page 3</NavLink></div>
+                    <NavWrapper><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page1"}>Page 1</NavLink></NavWrapper>
+                    <NavWrapper><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page2"}>Page 2</NavLink></NavWrapper>
+                    <NavWrapper><NavLink className={({isActive})=> isActive ? styles.active : styles.navLink} to={"/page3"}>Page 3</NavLink></NavWrapper>
                 </div>
                 <div className={styles.content}>
                   <Routes>
@@ -31,3 +31,19 @@ export const Site = () => {
     );
 };
 
+const NavWrapper = styled.div `
+margin-left: 10px;
+font-size: 50px;
+        
+& > a {
+    text-decoration: none;
+    color: #1e3786;
+}
+& > a.active {
+    text-decoration: none;
+    color: #03eaff;
+}
+& > a:hover {
+    color: steelblue;
+}
+`
