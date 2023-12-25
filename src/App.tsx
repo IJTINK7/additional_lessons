3ea6e44ebbae0905bb42165492fa01d297d60258
IJTINK7 <ijtink7@mail.ru>
2023-12-25 23:54:48 +0300

    Preparing for the 03-3 Additional lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
-import {NavLink, Outlet} from "react-router-dom";
-import styles from "./components/Site.module.css";
-import {s} from "./components/pages/_styles";
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
+import {AddItemForm} from './AddItemForm';
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
+import {
+    addTodolistAC
+} from './state/todolists-reducer';
+
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from './state/store';
+
+export type FilterValuesType = "all" | "active" | "completed";
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
+
 
 function App() {
-    const [burger, setBurger] = useState(true)
-    return (
-        <div>
-            <div>
-                <div className={styles.header}><h1>HEADER</h1></div>
-                <div className={styles.body}>
-                    {burger
-                    ? <div className={styles.nav}>
-                            <s.NavWrapper><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink}
-                                                   to={"/page/0"}>Page 1</NavLink></s.NavWrapper>
-                            <s.NavWrapper><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink}
-                                                   to={"/page/1"}>Page 2</NavLink></s.NavWrapper>
-                            <s.NavWrapper><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink}
-                                                   to={"/page/2"}>Page 3</NavLink></s.NavWrapper>
-                            <s.NavWrapper><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink}
-                                                   to={"/protected"}>Protected Page</NavLink></s.NavWrapper>
-                        </div>
-                    :  <div></div>
-                    }
 
-                    <div className={styles.content}>
-                        <Outlet/>
-                    </div>
-                </div>
-            </div>
+    const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
+    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+
+    const dispatch = useDispatch();
+
+    function addTodolist(title: string) {
+        addTodolistAC(title)
+    }
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: "20px"}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+                <Grid container spacing={3}>
+                    {
+                        todolists.map(tl => {
+                            let allTodolistTasks = tasks[tl.id];
+                            let tasksForTodolist = allTodolistTasks;
+
+                            if (tl.filter === "active") {
+                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+                            }
+                            if (tl.filter === "completed") {
+                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+                            }
+
+                            return <Grid item key={tl.id}>
+                                <Paper style={{padding: "10px"}}>
+                                    <Todolist
+                                        id={tl.id}
+                                        title={tl.title}
+                                        tasks={tasksForTodolist}
+                                        filter={tl.filter}
+                                    />
+                                </Paper>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Container>
         </div>
     );
 }
