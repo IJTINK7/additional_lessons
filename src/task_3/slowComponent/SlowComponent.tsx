466318c71169dc3a6fef8dd94118f5fcc33150a9
IJTINK7 <ijtink7@mail.ru>
2024-01-23 21:51:01 +0300

    Formatting the code

--- src/task_3/slowComponent/SlowComponent.tsx
+++ src/task_3/slowComponent/SlowComponent.tsx
@@ -1,25 +1,25 @@
 import {ChangeEvent, memo, useMemo, useState} from "react";
 
 export const SlowComponent = memo(() => {
-  console.log('SlowComponent re-render...');
+	console.log('SlowComponent re-render...');
 
-  const [value, setValue] = useState('');
+	const [value, setValue] = useState('');
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
+	const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
- useMemo(() => {
-    let now = performance.now();
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	useMemo(() => {
+		let now = performance.now();
 
-    while (performance.now() - now < 1000) {
-      // Artificial delay -- do nothing for 100ms
-    }
-  }, []);
+		while (performance.now() - now < 1000) {
+			// Artificial delay -- do nothing for 100ms
+		}
+	}, []);
 
-  return (
-      <div>
-        <input type="text" value={value} onChange={onChange} />
-        <p>I am a very slow component tree.</p>
-      </div>
-  );
+	return (
+		<div>
+			<input type="text" value={value} onChange={onChange}/>
+			<p>I am a very slow component tree.</p>
+		</div>
+	);
 });
