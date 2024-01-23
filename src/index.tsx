99445d3e8f09c18ac40f2b69cc043eada8c82035
IJTINK7 <ijtink7@mail.ru>
2024-01-23 20:18:15 +0300

    Starting 2.2 task

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {App} from './App';
+import ReactDOM from 'react-dom';
+import {App} from 'App';
 import reportWebVitals from './reportWebVitals';
 import {createRoot} from "react-dom/client";
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(<App/>);
+
 
 reportWebVitals();
