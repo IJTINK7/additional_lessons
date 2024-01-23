2ec271abf597c50ea83ee5903a01c10da2cbf208
IJTINK7 <ijtink7@mail.ru>
2024-01-23 21:39:02 +0300

    Finishing 2.3 task

--- src/task_3/slowComponent/SlowComponent.tsx
+++ src/task_3/slowComponent/SlowComponent.tsx
@@ -1,11 +1,25 @@
+import {ChangeEvent, useMemo, useState} from "react";
+
 export const SlowComponent = () => {
   console.log('SlowComponent re-render...');
 
-  let now = performance.now();
+  const [value, setValue] = useState('');
 
-  while (performance.now() - now < 1000) {
-    // Artificial delay -- do nothing for 100ms
-  }
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
 
-  return <p>I am a very slow component tree.</p>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+ useMemo(() => {
+    let now = performance.now();
+
+    while (performance.now() - now < 1000) {
+      // Artificial delay -- do nothing for 100ms
+    }
+  }, []);
+
+  return (
+      <div>
+        <input type="text" value={value} onChange={onChange} />
+        <p>I am a very slow component tree.</p>
+      </div>
+  );
 };
