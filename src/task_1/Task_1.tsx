536616f03319c8ee16e6d0a2f6bf74c31ef240e5
IJTINK7 <ijtink7@mail.ru>
2024-01-23 20:15:59 +0300

    Finishing 2.1 task

--- src/task_1/Task_1.tsx
+++ src/task_1/Task_1.tsx
@@ -1,32 +1,38 @@
-import {ChangeEvent, FC, useCallback, useState} from 'react';
+import {ChangeEvent, memo, useState} from 'react';
 
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
 
-type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 // Task 1
 // If change value on input Title component not re-render
 // If change value on input Task_1 component not re-render
 
 export const Task_1 = () => {
+  console.log("Task_1")
+
+  return (
+    <div style={{...CONTAINER_STYLES} as any}>
+      <Input />
+      <Title title="I am a title" />
+    </div>
+  );
+};
+
+const Title = memo((props: { title: string }) => {
+  console.log("Title was render")
+      return  <h3>{props.title}</h3>;
+})
+
+const Input = memo(() => {
   const [value, setValue] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value);
   };
 
-  return (
-    <div style={{...CONTAINER_STYLES} as any}>
-      <Input value={value} onChange={handleChange} />
-      <Title title="I am a title" />
-    </div>
-  );
-};
+  console.log("Input was render")
 
-const Title = (props: { title: string }) => <h3>{props.title}</h3>;
-
-const Input: FC<PropsType> = ({value, onChange}) => {
   return (
-    <input type="text" placeholder="Placeholder" value={value} onChange={onChange} />
+    <input type="text" placeholder="123" value={value} onChange={handleChange} />
   );
-};
+});
