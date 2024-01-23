c6fce8b52489677de38e48ad4333033929e62f04
IJTINK7 <ijtink7@mail.ru>
2024-01-23 20:57:51 +0300

    Finishing 2.2 task

--- src/task_2/Task_2.tsx
+++ src/task_2/Task_2.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from 'react';
+import {FC, memo, useCallback, useState} from 'react';
 
 const FIRST_BUTTON_BACKGROUND = {background: 'tomato'};
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
@@ -11,23 +11,25 @@ type PropsType = { isChecked: boolean, onSetIsChecked: () => void };
 // If click to Checkbox component Button not re-render
 
 export const Task_2 = () => {
+    console.log("Task_2 was render")
   const [firstCount, setFirstCount] = useState(0);
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleSetIsChecked = () => setIsChecked(!isChecked);
+  const handleSetIsChecked = useCallback(() => setIsChecked(!isChecked), [isChecked]);
 
-  const handlePlusCountValueClick = () => setFirstCount(prevFirstCount => prevFirstCount + 1);
+  const handlePlusCountValueClick = useCallback(() => setFirstCount(prevFirstCount => prevFirstCount + 1), []);
 
   return (
     <div style={{...CONTAINER_STYLES} as any}>
       <div>{`Count equal: ${firstCount}`}</div>
-      <Button onPlusCountValueClick={handlePlusCountValueClick} />
-      <Checkbox isChecked={isChecked} onSetIsChecked={handleSetIsChecked} />
+      <Button onPlusCountValueClick={handlePlusCountValueClick}/>
+      <Checkbox isChecked={isChecked} onSetIsChecked={handleSetIsChecked}/>
     </div>
   );
 };
 
-export const Button: FC<{ onPlusCountValueClick: () => void }> = ({onPlusCountValueClick}) => {
+export const Button: FC<{ onPlusCountValueClick: () => void }> = memo(({onPlusCountValueClick}) => {
+    console.log("Button was render")
   return (
     <div>
       <button
@@ -38,10 +40,11 @@ export const Button: FC<{ onPlusCountValueClick: () => void }> = ({onPlusCountVa
       </button>
     </div>
   );
-};
+});
 
-export const Checkbox: FC<PropsType> = ({isChecked, onSetIsChecked}) => {
+export const Checkbox: FC<PropsType> = memo(({isChecked, onSetIsChecked}) => {
+    console.log("Checkbox was render")
   return (
     <input type="checkbox" checked={isChecked} onChange={onSetIsChecked} />
   );
-};
+});
